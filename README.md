49b29597de5455bcbfba409892a277eba244d046
Jiuhong <jiuhong@casperlabs.io>
2023-02-10 11:50:04 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,56 @@
 # transfer-from-purse
 The main source includes the following steps
-1. create purse
-2. fund purse
-3. store purse into contract named_keys
-4. Create entry point
+## 1. create purse:
+### This step created a new purse under the account context
+```
+let second_purse = system::create_purse();
+```
+## 2. fund purse
+### This step transfers amount from account's main purse to the newly created purse.
+```
+let source: URef = account::get_main_purse();
+...
+system::transfer_from_purse_to_purse(source, second_purse, amount, None).unwrap_or_revert();
+```
+## 3. store purse into contract named_keys
+### This step stores newly created purse into the contract's named_keys
+```
+let mut named_keys: BTreeMap<String, Key> = BTreeMap::new();
+
+//store purse into contract's named_keys
+named_keys.insert(String::from("second_purse"), second_purse.into());
+```
+## 4. entry point
    - get target account hash
+   ```
+   let account_hash_key: Key = runtime::get_named_arg("account_hash");
+    let target_account_hash = account_hash_key.into_account().unwrap();
+   ```
    - transfer amount from created purse to target public key
+   ```
+   system::transfer_from_purse_to_account(source_purse, target_account_hash, amount, None)
+        .unwrap_or_revert();
+   ```
+
+## Command example
+### 1. install contract
+```
+casper-client put-deploy \
+--chain-name casper-test \
+--node-address http://94.130.10.55:7777 \
+--payment-amount 20000000000 \
+--session-path /home/jh/mywork/transfer-from-purse/contract/target/wasm32-unknown-unknown/release/contract.wasm \
+--session-arg "amount:U512='9000000000'" \
+--secret-key /home/jh/keys/test2/secret_key.pem 
+```
+
+### 2. call entrypoint `transfer_amount`
+```
+casper-client put-deploy --chain-name casper-test \
+--node-address http://94.130.10.55:7777 \
+--secret-key /home/jh/keys/test2/secret_key.pem \
+--session-hash "hash-01efcdd4aada8f161802c6f603544966d36c7c72c636da323346eb5e397c544c" \
+--session-entry-point "transfer_amount"  \
+--session-arg "account_hash:key='account-hash-d9758b25962f4cba82ba0047389af97a70acb7df43b391f9ffb293801bea5061'" \
+--payment-amount 3000000000
+```
--- contract/src/main.rs
+++ contract/src/main.rs
@@ -9,24 +9,22 @@ use casper_contract::{
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_types::{
-    account::AccountHash, CLType, EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, Key,
-    URef, U512,
+    CLType, EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, Key, URef, U512,
 };
 
 #[no_mangle]
 pub fn transfer_amount() {
-    let source_purse = runtime::get_key("secondpurse")
+    let source_purse = runtime::get_key("second_purse")
         .unwrap()
         .into_uref()
         .unwrap();
 
-    let target = AccountHash::from_formatted_str(
-        "account-hash-e65c2441d78fcf1f310db0c917dbfb54fdcc8f9b8c154d2beb78f2002079ad09",
-    )
-    .unwrap();
+    let account_hash_key: Key = runtime::get_named_arg("account_hash");
+    let target_account_hash = account_hash_key.into_account().unwrap();
     let amount = U512::from(1000000000);
 
-    system::transfer_from_purse_to_account(source_purse, target, amount, None).unwrap_or_revert();
+    system::transfer_from_purse_to_account(source_purse, target_account_hash, amount, None)
+        .unwrap_or_revert();
 }
 
 #[no_mangle]
@@ -35,19 +33,19 @@ pub extern "C" fn call() {
     let source: URef = account::get_main_purse();
 
     //create purse
-    let secondpurse = system::create_purse();
+    let second_purse = system::create_purse();
 
     //fund purse
-    system::transfer_from_purse_to_purse(source, secondpurse, amount, None).unwrap_or_revert();
+    system::transfer_from_purse_to_purse(source, second_purse, amount, None).unwrap_or_revert();
 
-    let mut counter_named_keys: BTreeMap<String, Key> = BTreeMap::new();
+    let mut named_keys: BTreeMap<String, Key> = BTreeMap::new();
 
-    //store purse into contract named_keys
-    counter_named_keys.insert(String::from("secondpurse"), secondpurse.into());
+    //store purse into contract's named_keys
+    named_keys.insert(String::from("second_purse"), second_purse.into());
 
     // Create entry point
-    let mut counter_entry_points = EntryPoints::new();
-    counter_entry_points.add_entry_point(EntryPoint::new(
+    let mut entry_points = EntryPoints::new();
+    entry_points.add_entry_point(EntryPoint::new(
         "transfer_amount",
         vec![],
         CLType::Unit,
@@ -56,14 +54,6 @@ pub extern "C" fn call() {
     ));
 
     let (stored_contract_hash, _) =
-        storage::new_locked_contract(counter_entry_points, Some(counter_named_keys), None, None);
-    runtime::put_key("transferamount", stored_contract_hash.into());
-
-    let target = AccountHash::from_formatted_str(
-        "account-hash-e65c2441d78fcf1f310db0c917dbfb54fdcc8f9b8c154d2beb78f2002079ad09",
-    )
-    .unwrap();
-
-    let amount1 = U512::from(1000000000u64);
-    system::transfer_from_purse_to_account(secondpurse, target, amount1, None).unwrap_or_revert();
+        storage::new_locked_contract(entry_points, Some(named_keys), None, None);
+    runtime::put_key("transfer_contract", stored_contract_hash.into());
 }
